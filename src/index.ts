6ac084211a546e943de1199923c0c2e8f7570757
Alex Ortiz-Rosado <alex@nana.io>
2022-11-04 13:30:18 -0700

    chore: setup linting standards

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+/***
+ * Typescript SDK for The Lord of the Rings API
+ *
+ * @author   Alex Ortiz-Rosado <alexortizrosado@gmail.com>
+ * @version  2022.00.01
+ * @link     https://github.com/alexortizrosado/alexortizrosado-SDK
+ */
