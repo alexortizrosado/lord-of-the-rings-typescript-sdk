7e838e6ebac4e2a904543b15570baaf6ab57f4fe
Alex Ortiz-Rosado <alex@nana.io>
2022-11-05 08:30:48 -0700

    fix: don't throw an error if item not found

--- src/Error.ts
+++ src/Error.ts
@@ -11,22 +11,10 @@ class UnauthorizedError extends Error {
   }
 }
 
-class NotFoundError extends Error {
-  code: ErrorCode;
-  constructor(message: string) {
-    super(message);
-    this.name = "NotFoundError";
-    this.code = 404;
-  }
-}
-
 export const generateError = (error: AxiosError<unknown, any>) => {
   const statusCode = error.response?.status;
   if (statusCode === 401) {
     const message = "Request requires an access token";
     throw new UnauthorizedError(message);
-  } else if (statusCode == 404) {
-    const message = "Unable to find item.";
-    throw new NotFoundError(message);
   }
 };
