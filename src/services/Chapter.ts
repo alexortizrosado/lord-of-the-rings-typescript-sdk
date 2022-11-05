be6ae39856914f7eb64bf302f56c074898fc0889
Alex Ortiz-Rosado <alex@nana.io>
2022-11-05 09:16:22 -0700

    feat: get chapters by book

--- src/services/Chapter.ts
+++ src/services/Chapter.ts
@@ -2,6 +2,7 @@ import { AxiosError, AxiosInstance } from "axios";
 import { generateError } from "../Error";
 import { ApiResponse } from "../types/ApiResponse";
 import { Chapter as ChapterType } from "../types/Chapter";
+import { Book } from "./Book";
 
 export class Chapter {
   private readonly client: AxiosInstance;
@@ -23,6 +24,15 @@ export class Chapter {
   async catalog(): Promise<ApiResponse<ChapterType[]>> {
     const response = await this.client
       .get(`${Chapter.basePath}`)
+      .catch((error: AxiosError) => {
+        generateError(error);
+      });
+    return response?.data;
+  }
+
+  async getChaptersByBook(bookId: string) {
+    const response = await this.client
+      .get(`${Book.basePath}/${bookId}/${Book.basePath}`)
       .catch((error: AxiosError) => {
         generateError(error);
       });
